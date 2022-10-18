9d2b31f19b59b43eb852c644870e76183cd84b3e
luis <lfscruz2001@gmail.com>
2022-10-18 16:59:28 -0300

    projeto criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +16,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </>
   );
 }
 
